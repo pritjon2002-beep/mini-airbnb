db534d1689af70bc9213daee8d9774a5afe9011b
pritam-demo <pritjon2002@gmail.com>
2026-09-09 17:38:35 +0545

    booking route created

---
+++ AirbnbFinal/routes/booking.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router({ mergeParams: true });
+const wrapAsync = require("../utils/wrapAsync.js");
+const { isLoggedIn } = require("../middleware.js");
+const bookingController = require("../controller/bookings.js");
+
+router.post("/", isLoggedIn, wrapAsync(bookingController.createBooking));
+
+module.exports = router;
