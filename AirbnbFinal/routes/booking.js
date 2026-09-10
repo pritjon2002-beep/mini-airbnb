96f6915bdf30a99cf955433d1855c18773d495c2
pritam-demo <pritjon2002@gmail.com>
2026-09-10 09:11:03 +0545

    simple fix

--- AirbnbFinal/routes/booking.js
+++ AirbnbFinal/routes/booking.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router({ mergeParams: true });
 const wrapAsync = require("../utils/wrapAsync.js");
 const { isLoggedIn } = require("../middleware.js");
-const bookingController = require("../controller/bookings.js");
+const bookingController = require("../controller/booking.js");
 
 router.post("/", isLoggedIn, wrapAsync(bookingController.createBooking));
 
